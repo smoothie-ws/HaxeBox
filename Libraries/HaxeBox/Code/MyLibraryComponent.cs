2d3864b0ced9c8f1d01e3f473e088eb0575fab18
smoothie-ws <kibot.email@gmail.com>
2026-01-28 22:23:39 +1000

    feat: upload sources

---
+++ Libraries/HaxeBox/Code/MyLibraryComponent.cs
@@ -0,0 +1,10 @@
+using Sandbox;
+
+/// <summary>
+/// This is a component - in your library!
+/// </summary>
+[Title( "HaxeBox - My Component" )]
+public class MyLibraryComponent : Component
+{
+
+}
